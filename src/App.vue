a4e5b19b3fe4af914c8f5ed803025ff8f1437f10
hfangjian <357987103@qq.com>
2022-04-03 20:58:59 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>This a hfj demo.</span>
   </div>
 </template>
 
